221e290c68991d877bbb5cbebd9fb99f76bc938e
Minh Dang <minhdang9a8@gmail.com>
2024-04-26 13:45:03 +0700

    complete view detail blogs by id functionality

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,5 +1,33 @@
+'use client';
+import { log } from 'console';
+import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import useSWR, { Fetcher } from 'swr';
 const ViewDetailBlog = ({ params }: { params: { id: string } }) => {
-    return <div>View Detail {params.id}</div>;
+    const fetcher: Fetcher<IBLOG, string> = (url: string) => fetch(url).then((res) => res.json());
+    // const fetcher = (url: string) => fetch(url).then((res) => res.json());
+    const { data, error, isLoading } = useSWR(`http://localhost:8000/blogs/${params.id}`, fetcher, {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false,
+    });
+    console.log('data', data);
+
+    return (
+        <div>
+            <div className="my-3">
+                <Link href={'/blogs'}>Go Back</Link>
+            </div>
+            <Card>
+                <Card.Header>Tiêu đề: {data?.title}</Card.Header>
+                <Card.Body>
+                    <Card.Text>{data?.content}</Card.Text>
+                </Card.Body>
+                <Card.Footer>Tác giả: {data?.author}</Card.Footer>
+            </Card>
+        </div>
+    );
 };
 
 export default ViewDetailBlog;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
             <body className={inter.className}>
                 <AppHeader />
-                <Container>{children}</Container>
+                <Container style={{ minHeight: 'calc(100vh - 106px' }}>{children}</Container>
 
                 <AppFooter />
                 <ToastContainer
